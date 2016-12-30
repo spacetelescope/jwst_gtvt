5d2b11280988ee915022e5c39f31eccd450d8069
Matthew Hill <mattjhill@users.noreply.github.com>
2016-12-30 16:09:49 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -65,11 +65,11 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[
-        'numpy',
-        'astropy',
-        'matplotlib',
-    ],
+#     install_requires=[
+#         'numpy',
+#         'astropy',
+#         'matplotlib',
+#     ],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
