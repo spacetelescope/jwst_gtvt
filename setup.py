6bc56f7e87b0f33ad2b6ca2b2f8b7e0b32ac1785
Mees Fix <mfix@stsci.edu>
2019-04-05 09:18:01 -0400

    Still working with setup.py. I think the astroquery problem has been solved

--- setup.py
+++ setup.py
@@ -46,8 +46,6 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
     ],
@@ -66,15 +64,16 @@ setup(
     # https://packaging.python.org/en/latest/requirements.html
      install_requires=[
          'numpy',
+         'astroquery',
          'astropy',
          'matplotlib',
          'requests',
      ],
 
     # Using bleeding edge of astroquery to get my changes for retrieving JWST ephemeris
-     dependency_links=[
-        'https://github.com/astropy/astroquery/'
-    ],
+    #  dependency_links=[
+    #     'https://github.com/astropy/astroquery/'
+    # ],
 
 
     # List additional groups of dependencies here (e.g. development
