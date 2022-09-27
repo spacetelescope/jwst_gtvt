e1736d5e5295e59b200534c2839dbbb8177ce730
Mees Fix <mfix@stsci.edu>
2022-09-27 09:37:00 -0400

    adding import for pi

--- jwst_gtvt/constants.py
+++ jwst_gtvt/constants.py
@@ -1,9 +1,14 @@
-D2R = pi/180.  #degrees to radians
-R2D = 180. / pi #radians to degrees 
-PI2 = 2. * pi   # 2 pi
+import numpy as np
+
+D2R = np.pi/180.  #  degrees to radians
+R2D = 180. / np.pi #  radians to degrees 
+PI2 = 2. * np.pi   #  2 pi
+
 UNIT_LIMIT = lambda x: min(max(-1.,x),1.) # forces value to be in [-1,1]
-MIN_SUN_ANGLE = 84.8 * D2R  #minimum Sun angle, in radians
-MAX_SUN_ANGLE = 135.0 * D2R #maximum Sun angle, in radians
-SUN_ANGLE_PAD = 0.5 * D2R   #pad away from Sun angle limits when constructing safe attitude
-OBLIQUITY_OF_THE_ECLIPTIC = -23.439291 * D2R # At J2000 equinox
-EPSIOLON = 23.43929 * D2R #obliquity of the ecliptic J2000
+
+MIN_SUN_ANGLE = 84.8 * D2R  #  minimum Sun angle, in radians
+MAX_SUN_ANGLE = 135.0 * D2R #  maximum Sun angle, in radians
+SUN_ANGLE_PAD = 0.5 * D2R   #  pad away from Sun angle limits when constructing safe attitude
+
+OBLIQUITY_OF_THE_ECLIPTIC = -23.439291 * D2R #  At J2000 equinox
+EPSIOLON = 23.43929 * D2R #  obliquity of the ecliptic J2000
