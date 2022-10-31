a18af3c851508a212b99a47fabe67a627f7723ab
Mees Fix <mfix@stsci.edu>
2022-10-31 15:05:24 -0500

    updating test

--- jwst_gtvt/tests/test_gtvt.py
+++ jwst_gtvt/tests/test_gtvt.py
@@ -35,7 +35,7 @@ class mimic_parser:
         self.save_table = None
         self.no_verbose = True
         self.start_date = '2021-12-26'
-        self.end_date = '2024-10-03'
+        self.end_date = '2024-10-02'
         self.v3pa = None
         self.instrument = None
         self.name = None
