a491f53649c531538857f0757d4c2da2beb6aafc
Mees Fix <mfix@stsci.edu>
2023-01-18 11:30:41 -0500

    Adding unit tests

--- jwst_gtvt/tests/test_gtvt.py
+++ jwst_gtvt/tests/test_gtvt.py
@@ -1,4 +1,6 @@
 """ JWST GENERAL AND MOVING TARGET VISIBILITY TOOL TESTS"""
+import pytest
+
 from jwst_gtvt.jwst_tvt import Ephemeris
 from jwst_gtvt.display_results import display_results
 from jwst_gtvt.plotting import plot_visibility
@@ -21,3 +23,16 @@ def test_ra_dec_sexigesimal():
     eph = Ephemeris()
     ra, dec = '253.2458', '2.4008'
     eph.get_fixed_target_positions(ra, dec)
+
+
+@pytest.mark.parametrize("instrument", ['MIRI', 'NIRCAM', 'NIRSPEC', 'NIRISS', 'FGS', 'V3PA'])
+@pytest.mark.parametrize("mode", ['fixed', 'moving'])
+def test_gtvt_single_instrument(instruments, mode):
+    eph = Ephemeris()
+    if mode == 'fixed':
+        ra, dec = '253.2458', '2.4008'
+        eph.get_fixed_target_positions(ra, dec)
+    else:
+        eph.get_moving_target_positions('Ceres', True)
+
+    plot_visibility(eph, instrument)
